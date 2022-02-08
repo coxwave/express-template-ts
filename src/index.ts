8a929e0c52d6dbf386996c4fc0e0d3041d2986fd
Kay Kim <kimjh@bawi.org>
2022-02-08 22:03:51 +0900

    🛠️ Use 'false' for default useHttps flag

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import 'module-alias/register';
 import * as fs from 'fs';
 import * as https from 'https';
 
-import { httpsPort, port, sslConf } from '@config/env';
+import { env, httpsPort, port, sslConf } from '@config/env';
 import { logger } from '@config/logger';
 
 import app from './app';
 
-const useHttps = true;
+const useHttps = env === 'production';
 
 app.listen(port, () => {
   logger.info(`HTTP SERVER LISTENING ON PORT ${port}`);
